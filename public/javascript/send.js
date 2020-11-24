ae304429dfe088023fc0b478a6d20c739d0c4d51
Keith <limbokeith71@gmail.com>
2020-11-24 14:57:46 +0800

    contact page

--- public/javascript/send.js
+++ public/javascript/send.js
@@ -16,7 +16,7 @@ var count = 0;
 var message = {
     notification: {
       title: 'S.R.O.Q.S',
-      body: 'You\'re next in queue! Please proceed to the office.'
+      body: 'You\'re next in queue! Please proceed to the office.',
     }
   };
 
@@ -24,11 +24,12 @@ var interval = setInterval(function() {
   var obj = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
   if(obj.token != ""){
     console.clear();
-    console.log(obj.token);
+    
     admin.messaging().sendToDevice(obj.token, message)
       .then((response) => {
       // Response is a message ID string.
       count++;
+      console.log(obj.token);
       console.log('Successfully sent message:', response);
       console.log("Number of Successfully sent messages: "+ count);
       })
@@ -37,6 +38,7 @@ var interval = setInterval(function() {
       });
   }
   else{
+    console.clear();
     console.log('data.json is empty');
   }
 }, 5000);
